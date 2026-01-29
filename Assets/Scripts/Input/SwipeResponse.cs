d199b4b9c47c920a006f23b204182e6b5a178f97
Colin Whiteford <colinwhiteford516@gmail.com>
2026-01-29 18:01:08 -0500

    Finally Fixed Tap Vs Swipe

--- Assets/Scripts/Input/SwipeResponse.cs
+++ Assets/Scripts/Input/SwipeResponse.cs
@@ -11,35 +11,52 @@ public class SwipeResponse
     Vector2 m_validSwipeDirection = Vector2.zero;
     float m_swipeValidDot;
     Vector2 m_inputValue;
-    [SerializeField] InputActionReference SwipeAction;
+    bool m_registeredTap;
+    [SerializeField] InputActionReference DeltaAction;
+    [SerializeField] InputActionReference HoldAction;
     [SerializeField] InputActionReference TapAction;
     [field: SerializeField] public UnityEvent Response { get; private set; } = new();
     public void Initialize()
     {
-        SwipeAction.action.Enable();
+        DeltaAction.action.Enable();
+        HoldAction.action.Enable();
         TapAction.action.Enable();
-        SwipeAction.action.performed += ProcessAction;
-        TapAction.action.canceled += ConfirmAction;
+        HoldAction.action.started += ClearTap;
+        DeltaAction.action.performed += ProcessAction;
+        TapAction.action.performed += RegisterTap;
+        HoldAction.action.canceled += ConfirmAction;
         m_validSwipeDirection = new(Mathf.Cos(m_swipeAngle * Mathf.Deg2Rad), Mathf.Sin(m_swipeAngle * Mathf.Deg2Rad));
         m_swipeValidDot = Mathf.Cos(m_swipeAngleThreshold * Mathf.Deg2Rad);
     }
     public void Cleanup()
     {
-        SwipeAction.action.performed -= ProcessAction;
-        TapAction.action.canceled -= ConfirmAction;
-        SwipeAction.action.Disable();
+        DeltaAction.action.performed -= ProcessAction;
+        HoldAction.action.canceled -= ConfirmAction;
+        TapAction.action.performed -= RegisterTap;
+        DeltaAction.action.Disable();
+        HoldAction.action.Disable();
         TapAction.action.Disable();
     }
     void ProcessAction(InputAction.CallbackContext context)
     {
-        m_inputValue += SwipeAction.action.ReadValue<Vector2>();
+        m_inputValue += DeltaAction.action.ReadValue<Vector2>();
     }
     void ConfirmAction(InputAction.CallbackContext context)
     {
-        if (m_inputValue.magnitude > m_swipeStrengthThreshold && Vector2.Dot(m_inputValue.normalized, m_validSwipeDirection.normalized) > m_swipeValidDot)
+        if (m_inputValue.magnitude > m_swipeStrengthThreshold && Vector2.Dot(m_inputValue.normalized, m_validSwipeDirection.normalized) > m_swipeValidDot && !m_registeredTap)
         {
             Response.Invoke();
         }
         m_inputValue = Vector2.zero;
+        m_registeredTap = false;
+    }
+
+    void RegisterTap(InputAction.CallbackContext context)
+    {
+        m_registeredTap = true;
+    }
+    void ClearTap(InputAction.CallbackContext context)
+    {
+        m_registeredTap = false;
     }
 }
